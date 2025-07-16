35dc77df0223b93fe695386da7b305a862d6fb54
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-07-16 18:48:35 +0100

    First attempt to create a shared lib
    Really bad, some stuffs needs to be reviewed

--- src/core/core.h
+++ src/core/core.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#ifdef MABBLE_SHARED
+  #ifdef MABBLE_BUILD
+    #define MABBLE_API __declspec(dllexport)
+  #else
+    #define MABBLE_API __declspec(dllimport)
+  #endif
+#else
+  #define MABBLE_API
+#endif
---
+++ src/core/temp.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace mabble {
+
+	void temp() {
+		std::cout << "This is a temporary function." << std::endl;
+	}
+
+}
