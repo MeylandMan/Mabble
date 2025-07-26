473df573c7d79f304f67eca9666b4066b2b354b1
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-07-26 14:20:59 +0100

    Added Buffer Layout and Buffer Element

--- include/mabble/mabble.h
+++ include/mabble/mabble.h
@@ -4,7 +4,7 @@
 
 #include "../src/core/temp.h"
 
-
+#include <vector>
 #include <cstdint>
 #include <cstring>
 
@@ -185,4 +185,77 @@ namespace Mabble
 			return 0;
 		}
 	}
+
+	struct BufferElement
+	{
+		std::string Name;
+		ShaderDataType Type;
+		uint32_t Size;
+		uint32_t Offset;
+		bool Normalized;
+		BufferElement() = default;
+		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false)
+			: Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
+		{
+		}
+		uint32_t GetComponentCount() const
+		{
+			switch (Type)
+			{
+			case ShaderDataType::Float:  return 1;
+			case ShaderDataType::Float2: return 2;
+			case ShaderDataType::FLoat3: return 3;
+			case ShaderDataType::Float4: return 4;
+			case ShaderDataType::Mat3:   return 3 * 3;
+			case ShaderDataType::Mat4:   return 4 * 4;
+			case ShaderDataType::Int:    return 1;
+			case ShaderDataType::Int2:   return 2;
+			case ShaderDataType::Int3:   return 3;
+			case ShaderDataType::Int4:   return 4;
+			case ShaderDataType::Bool:   return 1;
+			default:
+				MABBLE_ASSERT(false && "Unknown ShaderDataType !");
+				return 0;
+			}
+		}
+	};
+
+	class MABBLE_API BufferLayout
+	{
+	public:
+		BufferLayout() {}
+
+		BufferLayout(std::initializer_list<BufferElement> elements)
+			: m_Elements(elements)
+		{
+			CalculateOffsetsAndStride();
+		}
+
+		uint32_t GetStride() const { return m_Stride; }
+		const std::vector<BufferElement>& GetElements() const { return m_Elements; }
+
+		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
+		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+		std::vector<BufferElement>::const_iterator begin() const { return m_Elements.begin(); }
+		std::vector<BufferElement>::const_iterator end() const { return m_Elements.end(); }
+		std::vector<BufferElement>::const_iterator cbegin() const { return m_Elements.cbegin(); }
+		std::vector<BufferElement>::const_iterator cend() const { return m_Elements.cend(); }
+	private:
+		void CalculateOffsetsAndStride()
+		{
+			uint32_t offset = 0;
+			m_Stride = 0;
+			for (auto& element : m_Elements)
+			{
+				element.Offset = offset;
+				offset += element.Size;
+				m_Stride += element.Size;
+			}
+		}
+	private:
+		std::vector<BufferElement> m_Elements;
+		uint32_t m_Stride = 0;
+	};
+
+
 }
