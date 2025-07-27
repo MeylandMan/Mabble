1261556ebfd984fa943423f97498e8361b0dfdcd
M.Meyland <125752061+MeylandMan@users.noreply.github.com>
2025-07-27 15:38:08 +0100

    Removed NOMINAX macro

--- tests/mbtpch.h
+++ tests/mbtpch.h
@@ -2,13 +2,6 @@
 
 #include "core/Platforms.h"
 
-#ifdef MABBLE_PLATFORM_WINDOWS
-	#ifndef NOMINMAX
-		// See github.com/skypjack/entt/wiki/Frequently-Asked-Questions#warning-c4003-the-min-the-max-and-the-macro
-		#define NOMINMAX
-	#endif
-#endif
-
 #include <iostream>
 #include <memory>
 #include <utility>
